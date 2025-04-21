25137e2629528b09f9c05ed4be22214c5bd8ac27
Sucharita Lahiri <sucharita14.lahiri@gmail.com>
2025-04-21 20:49:22 +0530

    New changes added

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -5,7 +5,7 @@ import useAutocomplete from "@mui/base/useAutocomplete";
 import { styled } from "@mui/system";
 import { truncate } from "../../helpers/helpers";
 import { useNavigate } from "react-router-dom";
-import { Tooltip } from "@mui/material";
+// import { Tooltip } from "@mui/material";
 
 const Listbox = styled("ul")(({ theme }) => ({
   width: "100%",
@@ -39,7 +39,7 @@ const Listbox = styled("ul")(({ theme }) => ({
 function Search({ searchData, placeholder }) {
   const {
     getRootProps,
-    getInputLabelProps,
+    // getInputLabelProps,
     value,
     getInputProps,
     getListboxProps,
